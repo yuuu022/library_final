a7e0d375c51d4d2a7626e2dd27b6ee8854ef3a53
linyu114 <11056006@ntub.edu.tw>
2024-01-10 13:51:12 +0800

    不需要一模一樣也能查詢

--- mysite/filters.py
+++ mysite/filters.py
@@ -1,9 +1,9 @@
 from .models import Book
 import django_filters
- 
- 
+
 class BookFilter(django_filters.FilterSet):
- 
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+
     class Meta:
         model = Book
         fields = '__all__'
